26bdc2a22765bbdc93e53ef2205d2d9961f3c0bb
Shaun Fowler <shaunfowler@users.noreply.github.com>
2023-01-19 15:54:48 -0500

    setup the table schema

---
+++ src/data/case-status.ts
@@ -0,0 +1,7 @@
+export interface CaseStatus {
+    receiptNumber: string
+    status: string
+    description: string
+    date: string | undefined
+    formType: string | undefined
+}
---
+++ src/my-cases/case-status-schema.ts
@@ -0,0 +1,22 @@
+export const columns = [
+    {
+        title: 'Receipt Number',
+        dataIndex: 'receiptNumber',
+        key: 'receiptNumber',
+    },
+    {
+        title: 'Form Type',
+        dataIndex: 'formType',
+        key: 'formType',
+    },
+    {
+        title: 'Date',
+        dataIndex: 'date',
+        key: 'date',
+    },
+    {
+        title: 'Status',
+        dataIndex: 'status',
+        key: 'status',
+    }
+]
